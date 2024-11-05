9edcca7e3434ea6872b184fe3d901fa4e5c3df85
Mhd Rafi <rafikpmty@gmail.com>
2024-11-05 19:18:19 +0530

    razorpay_updated

--- controllers/checkoutController.js
+++ controllers/checkoutController.js
@@ -7,6 +7,7 @@ import Wallet from "../models/walletModel.js";
 import Coupon from "../models/couponModel.js";
 import Razorpay from "razorpay";
 
+dotenv.config();
 
 const { RAZORPAY_ID_KEY, RAZORPAY_SECRET_KEY } = process.env;
 
